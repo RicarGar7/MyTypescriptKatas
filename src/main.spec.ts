4a499d5f29cdc945a5eaab09c1b429c5d35eda6f
ricargar7 <ricardo.garcia@leanmind.es>
2023-04-14 15:22:37 +0100

    Iteration with Sara

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,39 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { describe, expect, it } from "vitest"
+import { interpolateValuesInTemplate, Replacements } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+describe("Template string", () => {
+  it.each([[""], ["Texto sin variable"]])("should do nothing given no interpolations", (template: string) => {
+    expect(interpolateValuesInTemplate(template, {})).toEqual(template)
+  })
 
-    expect(result).toBe(true)
+  it.each([
+    ["${variable}", { variable: "valor" }, "valor"],
+    ["This is a template with one ${variable}", { variable: "foo" }, "This is a template with one foo"],
+    [
+      "This is a text with a ${variable} to be replaced. \n" +
+        "And this is another text with ${other-variable} to be replaced. \n" +
+        "And this is another text with ${another-variable} to be replaced.",
+      { variable: "value", "other-variable": "other-value", "another-variable": "another-value" },
+      "This is a text with a value to be replaced. \n" +
+        "And this is another text with other-value to be replaced. \n" +
+        "And this is another text with another-value to be replaced.",
+    ],
+    ["${variable} ${variable} ${variable}", { variable: "foo" }, "foo foo foo"],
+  ])(
+    "should replace interpolation keys in text with values from dictionary",
+    (template: string, dictionary: Replacements, expected: string) => {
+      const result = interpolateValuesInTemplate(template, dictionary)
+      expect(result).toEqual(expected)
+    },
+  )
+
+  it.each([
+    // ["This is a template with one ${variable}", {}, Exception],
+    // ["This is a template with ozne variable", { variable: "foo" }, "This is a template with one variable"],
+    ["${variable} ${variable2}", { variable: "foo", variable2: "${variable}" }, "foo ${variable}"],
+    ["${variable} ${variable2}", { variable: "${variable2}", variable2: "foo" }, "${variable2} foo"],
+  ])("should handle edge cases", (template: string, dictionary: Replacements, expected: string) => {
+    const result = interpolateValuesInTemplate(template, dictionary)
+    expect(result).toEqual(expected)
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
-export function myFunction() {
-  return true
+export type Replacements = { [key: string]: string }
+
+export function interpolateValuesInTemplate(template: string, interpolationValues: Replacements): string {
+  for (const [key, value] of Object.entries(interpolationValues)) {
+    template = template.replaceAll("${" + key + "}", value)
+  }
+  return template
 }
