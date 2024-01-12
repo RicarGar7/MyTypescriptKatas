56c69ef1d44b41e97daf44be61b127c88cb2266c
ricargar7 <ricardo.garcia@leanmind.es>
2024-01-12 12:24:59 +0000

    🔴Sum inserted amount

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,30 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import fc from "fast-check";
+import { describe, expect, it } from "vitest";
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+enum Coin{
+  FIVE = 5
+}
+class VendingMachine {
+  amount: number= 0;
 
-    expect(result).toBe(true)
-  })
-})
+  insert(coin: Coin) {
+
+  }
+}
+
+describe("Vending machine kata should", () => {
+  /*Todo:
+    - Feature
+         Accept coins
+         - Insert permitted coins
+         - Insert non-permitted coins
+
+  */
+  it("Sum inserted amount", () => {
+    const machine = new VendingMachine()
+    machine.insert(Coin.FIVE)
+    expect(machine.amount).toBe(5)
+  });
+
+
+});
